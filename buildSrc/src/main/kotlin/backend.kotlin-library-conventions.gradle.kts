084f7278603f21aa5788bb3669314fb322a6f665
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-10-09 20:34:52 +0800

    Init backend gradle projects (#2)

---
+++ buildSrc/src/main/kotlin/backend.kotlin-library-conventions.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("backend.kotlin-common-conventions")
+
+    `java-library`
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "backend"
+
+include("server-api")
+include("db-wrapper")
+include("tinkoff-wrapper")
+include("bot-service")
+include("statistics-service")
+include("strategy-service")
+include("user-service")
