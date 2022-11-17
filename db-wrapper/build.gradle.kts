9d5557b767850efb5fc4a974a46cc1be0931dd1e
bagrorg <54815354+bagrorg@users.noreply.github.com>
2022-11-17 22:36:55 +0300

    + config reading (#16)

--- db-wrapper/build.gradle.kts
+++ db-wrapper/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
     implementation("org.slf4j:slf4j-simple:1.7.9")
     implementation("org.postgresql:postgresql:42.2.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.0")
 }
---
+++ db-wrapper/src/main/kotlin/backend/db/common/DatabaseConfig.kt
@@ -0,0 +1,21 @@
+package backend.db.common
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import java.nio.file.Paths
+
+data class DatabaseCredentials(val host: String = "localhost", val port: Int = 1234, val user: String = "postgres", val pass: String = "")
+data class DatabaseConfig(val database: DatabaseCredentials = DatabaseCredentials())
+
+fun getDatabaseConfig(): DatabaseConfig {
+    val env = System.getenv("ATDB_PATH") ?: { "" }
+    val f = Paths.get("$env/conf.yaml").toFile()
+    if (!f.exists()) {
+        return DatabaseConfig()
+    }
+
+    val mapper = ObjectMapper(YAMLFactory())
+    mapper.findAndRegisterModules()
+
+    return mapper.readValue(f, DatabaseConfig::class.java)
+}
