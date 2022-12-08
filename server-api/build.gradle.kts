abd47208b17e5db6380ddb24a4cc0abcd6e2d68e
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-08 22:12:50 +0300

    add dependencies

--- server-api/build.gradle.kts
+++ server-api/build.gradle.kts
@@ -16,10 +16,12 @@ dependencies {
     implementation(project(":strategy-service"))
     implementation(project(":statistics-service"))
 
-    implementation("io.ktor:ktor-server-netty:1.5.2")
     implementation("io.ktor:ktor-html-builder:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
     implementation("io.ktor:ktor-serialization:1.5.2")
+    implementation("io.ktor:ktor-server-auth:1.5.2")
+    implementation("io.ktor:ktor-server-auth-jwt:1.5.2")
+    implementation("io.ktor:ktor-server-netty:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
 
     // Swagger
     implementation("com.github.nielsfalk:ktor-swagger:v0.7.0")
